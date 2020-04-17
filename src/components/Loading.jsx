a2326db3b3f86dd7b97bb893b52b1956e2731fcb
blueca <nickoldacre@outlook.com>
2020-04-17 09:44:24 +0100

    clean unneeded imports

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,14 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
-import { col } from '../styles/colours';
-
-const StyledDiv = styled.div`
-  padding: 0.5rem;
-  margin: 0.5rem;
-  background-color: ${col.layer1};
-  border-radius: 0.3rem;
-  box-shadow: 0 0 0.2rem black;
-`;
 
 const Loading = () => {
   return (
